329a07a43a261cc384b63cb2e32a16822e7bc80e
Diego <dcayo.156@gmail.com>
2019-09-02 09:29:49 -0400

    Cambios Diego 02-09-19
    Reportes

--- DinoM/PR_StockActualProductosLotes.vb
+++ DinoM/PR_StockActualProductosLotes.vb
@@ -25,24 +25,75 @@ Public Class PR_StockActualProductosLotes
     End Property
 
 #End Region
-
+#Region "Eventos"
     Private Sub _prCargarReporte()
-        Dim _dt As New DataTable
-
-        Dim objrep As New R_StockActualLote()
-
-        _dt = L_fnStockActualLote()
-
-        objrep.SetDataSource(_dt)
-        'MReportViewer
-        MReportViewer.ReportSource = objrep
 
     End Sub
     Private Sub PR_StockActual_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        _prIniciarTodo()
+    End Sub
+    Private Sub btnGenerar_Click(sender As Object, e As EventArgs) Handles btnGenerar.Click
+        _prBuscar()
+    End Sub
+
+    Private Sub checkTodosGrupos_CheckValueChanged(sender As Object, e As EventArgs) Handles checkTodosGrupos.CheckValueChanged
+        If (checkTodosGrupos.Checked) Then
+            cbGrupos.Enabled = False
+        Else
+            cbGrupos.Enabled = True
+        End If
+    End Sub
+
+    Private Sub CheckBoxX1_CheckValueChanged(sender As Object, e As EventArgs) Handles chbTodos.CheckValueChanged
+        If (chbTodos.Checked) Then
+            tbValor.IsInputReadOnly = True
+            tbValor.ResetText()
+        Else
+            tbValor.IsInputReadOnly = False
+            tbValor.Select()
+        End If
+    End Sub
+#End Region
+
+#Region "Metodos"
+    Private Sub _prIniciarTodo()
         'L_prAbrirConexion()
         MReportViewer.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None
         Me.WindowState = FormWindowState.Maximized
         Me.Text = pTitulo
         _prCargarReporte()
+        _prCargarComboGrupos(cbGrupos)
+        Me.ToolTip1.SetToolTip(SymbolBox1, "Permite consultar todos los productos que tengan una fecha de vencimiento proxima según el parámetro específicado")
+        Me.ToolTip1.IsBalloon = True
     End Sub
+    Private Sub _prBuscar()
+        Dim _dt As New DataTable
+        _dt = L_fnBuscarStockLote(cbGrupos.Value, checkTodosGrupos.Checked, tbValor.Value, chbTodos.Checked)
+        Dim objrep As New R_StockActualLote()
+        objrep.SetDataSource(_dt)
+        'MReportViewer
+        MReportViewer.ReportSource = objrep
+    End Sub
+    Private Sub _prCargarComboGrupos(mCombo As Janus.Windows.GridEX.EditControls.MultiColumnCombo)
+        Dim dt As New DataTable
+        dt = L_fnObtenerGruposLibreria()
+        'a.ylcod2,yldes2
+        With mCombo
+            .DropDownList.Columns.Clear()
+            .DropDownList.Columns.Add("yccod3").Width = 60
+            .DropDownList.Columns("yccod3").Caption = "COD"
+            .DropDownList.Columns.Add("yldes2").Width = 250
+            .DropDownList.Columns("yldes2").Caption = "GRUPOS"
+            .ValueMember = "yccod3"
+            .DisplayMember = "yldes2"
+            .DataSource = dt
+            .Refresh()
+        End With
+        If (dt.Rows.Count > 0) Then
+            cbGrupos.SelectedIndex = 0
+        End If
+    End Sub
+
+
+#End Region
 End Class
